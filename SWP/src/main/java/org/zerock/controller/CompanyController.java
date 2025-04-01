c5b8e36ab901372785aa5df01a742631abf54103
holyrootx <to_be_it_dinosaur@naver.com>
2025-04-02 01:09:09 +0900

    <add: 모든 Controller에 Annotation 추가 >
    ❗❗ Controller
    ❗❗ @RequestMapping("/product/*")부분 변경하여 사용 요망 ❗❗

--- SWP/src/main/java/org/zerock/controller/CompanyController.java
+++ SWP/src/main/java/org/zerock/controller/CompanyController.java
@@ -1,4 +1,13 @@
 package org.zerock.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product/*")
+@Log4j
+@AllArgsConstructor
 public class CompanyController {
 }
