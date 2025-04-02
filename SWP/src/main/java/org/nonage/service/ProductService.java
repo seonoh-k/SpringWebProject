67edc0cf95e7aecd81109a25ece0f5e04ed2bb81
seonoh.kim <kimseonoh101@gmail.com>
2025-04-02 20:14:50 +0900

    <1일차 끗>

--- SWP/src/main/java/org/nonage/service/ProductService.java
+++ SWP/src/main/java/org/nonage/service/ProductService.java
@@ -8,4 +8,16 @@ public interface ProductService {
 
     public ArrayList<ProductVO> getProductList (int page, int limit);
 
+    public void insert (ProductVO product);
+
+    public void modify (ProductVO product);
+
+    public ProductVO getDetail (int pseq);
+
+    public ArrayList<ProductVO> getNewList();
+
+    public ArrayList<ProductVO> getBestList();
+
+    public ArrayList<ProductVO> getKindList(String kind);
+
 }
--- SWP/src/main/java/org/nonage/service/ProductServiceImpl.java
+++ SWP/src/main/java/org/nonage/service/ProductServiceImpl.java
@@ -1,7 +1,10 @@
 package org.nonage.service;
 
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.nonage.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.nonage.domain.ProductVO;
 
@@ -11,8 +14,54 @@ import java.util.ArrayList;
 @AllArgsConstructor
 @Log4j
 public class ProductServiceImpl implements ProductService{
+
+    @Setter(onMethod_ = @Autowired)
+    private ProductMapper mapper;
+
     @Override
     public ArrayList<ProductVO> getProductList(int page, int limit) {
         return null;
+    }
+
+    @Override
+    public void insert(ProductVO product) {
+
+    }
+
+    @Override
+    public void modify(ProductVO product) {
+
+    }
+
+    @Override
+    public ProductVO getDetail(int pseq) {
+
+        log.info("pseq : " + pseq);
+
+        return mapper.getDetail(pseq);
+    }
+
+    @Override
+    public ArrayList<ProductVO> getNewList() {
+
+        log.info("get new list");
+
+        return mapper.getNewList();
+    }
+
+    @Override
+    public ArrayList<ProductVO> getBestList() {
+
+        log.info("get best list");
+
+        return mapper.getBestList();
+    }
+
+    @Override
+    public ArrayList<ProductVO> getKindList(String kind) {
+
+        log.info("service kind : " + kind);
+
+        return mapper.getKindList(kind);
     }
 }
